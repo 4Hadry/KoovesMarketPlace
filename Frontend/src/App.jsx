e75f5c0b6bc7958e47903f6c4f6da65889d22fe0
4Hadry <chammar234@gmail.com>
2024-06-07 15:50:11 +0500

    Product Detail Page Designing Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Cart from "./pages/Cart";
 import Search from "./pages/Search";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import ProductDetail from "./pages/ProductDetail";
 // import Cart from "./pages/cart";
 // Pages
 // const Home = lazy(() => import("./pages/Home"));
@@ -25,6 +26,7 @@ const App = () => {
           <Route path="/search" element={<Search />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<SignUp />} />
+          <Route path="/productDtails" element={<ProductDetail />} />
         </Routes>
       </BrowserRouter>
     </>

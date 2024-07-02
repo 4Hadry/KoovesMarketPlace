ca5e87bc88a49709336eb02c5507dba2a19a1d85
4Hadry <chammar234@gmail.com>
2024-07-02 14:42:10 +0500

    some fixes

--- Frontend/src/pages/admin/dashboard.jsx
+++ Frontend/src/pages/admin/dashboard.jsx
@@ -10,6 +10,9 @@ import Table from "../../components/admin/DashboardTable";
 import { useStatsQuery } from "../../redux/api/dashboardApi";
 import { Skeleton } from "../Loader";
 import { Navigate } from "react-router-dom";
+import { getLastMonths } from "../../utils/features";
+
+const { last6Months } = getLastMonths();
 
 const userImg =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJxA5cTf-5dh5Eusm0puHbvAhOrCRPtckzjA&usqp";
@@ -69,6 +72,7 @@ const Dashboard = () => {
               <div className="revenue-chart">
                 <h2>Revenue & Transaction</h2>
                 <BarChart
+                  labels={last6Months}
                   data_1={stats.chart.order}
                   data_2={stats.chart.order}
                   title_1="Revenue"
